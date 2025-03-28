e5ebbc630e570684e97b3b9d247c9117324957b2
Aliza Ikram <alizaikram3@gmail.com>
2025-03-28 17:11:42 +0500

    Add LocalStorage funcionality and custom alert box

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const completedList = document.querySelector(".todo-list.completed");
 const incompleteList = document.querySelector(".todo-list.incomplete");
 const ul = document.querySelector(".todo-list");
 
+let todoList = [];
+
+
 addBtn.addEventListener("click", addTask);
 
 taskInput.addEventListener("keydown", (e) => {
@@ -15,7 +18,7 @@ taskInput.addEventListener("keydown", (e) => {
 function addTask() {
   
     if (taskInput.value.trim() === "") {
-      alert("Please enter a task");
+      showCustomAlert("Please enter a task");
       return;
     } 
 
@@ -28,12 +31,16 @@ function addTask() {
               <img src="assets/delete_24dp_C89F7E_FILL0_wght400_GRAD0_opsz24.svg" alt="delete" class="delete-icon"/>
               <div class="checkbox"></div>
             </div>`;
-
+      
+            todoList.push({text: `${task}`, completed: false});
+            
       ul.appendChild(li);
 
       taskInput.value = "";
-  
+      saveTodoList();
 }
+
+
 
 
 function editTask() {
@@ -45,6 +52,7 @@ function editTask() {
       if (e.target.alt === "edit") {
         liContent.setAttribute("contenteditable", "true");
         liContent.focus();
+
         // Move cursor to the end of the content
         const range = document.createRange();
         range.selectNodeContents(liContent);
@@ -52,6 +60,7 @@ function editTask() {
         const selection = window.getSelection();
         selection.removeAllRanges();
         selection.addRange(range);
+
         // Change the icon
         e.target.src = "assets/save_24dp_C89F7E_FILL0_wght400_GRAD0_opsz24.svg";
         e.target.alt = "save";
@@ -63,11 +72,13 @@ function editTask() {
           e.target.src =
             "assets/border_color_24dp_C89F7E_FILL0_wght400_GRAD0_opsz24.svg";
           e.target.alt = "edit";
+          saveTodoList();
         }
       }
     }
   });
 }
+
 
 function deleteTask() {
   [incompleteList, completedList].forEach((list) => {
@@ -75,6 +86,10 @@ function deleteTask() {
       if (e.target.classList.contains("delete-icon")) {
         const li = e.target.closest("li");
         li.remove();
+        
+        const index = todoList.indexOf(li);
+        todoList.splice(index, 1);
+        saveTodoList();
       }
     });
   });
@@ -85,10 +100,20 @@ function taskCompleted() {
     if (e.target.classList.contains("checkbox")) {
       const li = e.target.closest("li");
       if (li.querySelector("span").getAttribute("contenteditable") === "true") {
-        alert("Please save the task first");
+        showCustomAlert("Please save the task first");
         return;
       }
       completedList.appendChild(li);
+      for (const index of todoList.keys()) {
+        if (todoList[index].text === li.querySelector("span").textContent) {
+          todoList[index].completed = true;
+        }
+
+      }
+
+      saveTodoList();
+      console.log(todoList);
+      
       const circle = li.querySelector(".checkbox");
       circle.classList.add("checked");
       circle.style.backgroundColor = "#c89f7e";
@@ -99,11 +124,19 @@ function taskCompleted() {
   });
 }
 
-function undoCompletedTask() {
+function undoCompletedTask(task) {
   completedList.addEventListener("click", (e) => {
     if (e.target.classList.contains("checked")) {
       const li = e.target.closest("li");
       incompleteList.appendChild(li);
+
+      for (const index of todoList.keys()) {
+        if (todoList[index].text === li.querySelector("span").textContent) {
+          todoList[index].completed = false;
+        }
+
+      }
+      saveTodoList();
 
       const editHidden = li.querySelector(".edit-icon.hidden-edit");
       editHidden.style.display = "flex";
@@ -113,6 +146,59 @@ function undoCompletedTask() {
     }
   });
 }
+
+function saveTodoList() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function loadTodoList() {
+  const storedTodoList = localStorage.getItem("todoList");
+  if(storedTodoList) {
+    todoList = JSON.parse(storedTodoList);
+    if(todoList.length > 0) {
+      todoList.forEach((task) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<span contenteditable="false">${task.text}</span>
+        <div class="actions">
+              <img src="assets/border_color_24dp_C89F7E_FILL0_wght400_GRAD0_opsz24.svg" alt="edit" class="edit-icon hidden-edit" />
+              <img src="assets/delete_24dp_C89F7E_FILL0_wght400_GRAD0_opsz24.svg" alt="delete" class="delete-icon"/>
+              <div class="checkbox"></div>
+            </div>`;
+            
+        if(task.completed === false) { 
+          incompleteList.appendChild(li);
+        } else {
+          
+          completedList.appendChild(li);
+          
+          const circle = li.querySelector(".checkbox");
+          circle.classList.add("checked");
+          circle.style.backgroundColor = "#c89f7e";
+        }
+      });
+    }
+  }
+}
+
+
+            const alertContainer = document.getElementById('custom-alert');
+            const alertMessage = document.getElementById('alert-message');
+            const closeButton = document.getElementById('close-button');
+
+            function showCustomAlert(message) {
+                alertMessage.textContent = message;
+                alertContainer.style.display = 'block';
+            }
+
+            function hideCustomAlert() {
+                alertContainer.style.display = 'none';
+            }
+
+            closeButton.addEventListener('click', hideCustomAlert);
+
+
+loadTodoList();
+
 
 editTask();
 deleteTask();
